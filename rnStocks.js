f7141c85f48e1e572a625af5cbde64b5845ec14f
James <jbkuczma@buffalo.edu>
2016-05-26 19:34:09 -0400

    adding a nav bar... i think

--- rnStocks.js
+++ rnStocks.js
@@ -20,16 +20,45 @@ import {
 
 import MainWindow from './App/Views/Main';
 
+var NavigationBarRoute = {
+    LeftButton(route, navigator, index, navState){
+        if(index > 0){
+            return(
+                <TouchableHighlight
+                    underlayColor='transparent'
+                    onPress={() => {if(index > 0){navigator.pop() } }}
+                >
+                <Text> Back </Text>
+                </TouchableHighlight>
+            )
+        }else{
+            return null;
+        }
+    },
+    RightButton(route, navigator, index, navState){
+        return null;
+    },
+    Title(route, navigator, index, navState){
+        return null;
+    }
+};
+
 class rnStocks extends React.Component {
 
     renderScene(route, navigator){
         return <route.component {...route.passProps} navigator = {navigator} />
     }
+
     render(){
         return(
             <Navigator
                 initialRoute={{name: 'Main', component: MainWindow}}
                 renderScene = {this.renderScene}
+                navigationBar={
+                    <Navigator.NavigationBar
+                        routeMapper={NavigationBarRoute}
+                    />
+                }
             />
         )
     }
