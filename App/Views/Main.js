713deac6b1d0f500f292744c22d082c27d53cb9f
James <jbkuczma@buffalo.edu>
2016-06-02 08:44:34 -0400

    trying to implement refresh control. having problems

--- App/Views/Main.js
+++ App/Views/Main.js
@@ -12,6 +12,7 @@ import {
   RefreshControl,
   TouchableWithoutFeedback,
   ScrollView,
+  Alert,
 } from 'react-native';
 
 import styles from '../Styles/styles';
@@ -35,9 +36,46 @@ class MainWindow extends React.Component {
         });
         this.state = {
             dataSource: ds.cloneWithRows(data),
+            refreshing: false,
+            db: data,
         };
         this.updateItem = this.updateItem.bind(this);
         this.openItem = this.openItem.bind(this);
+    }
+
+    componentWillMount(){
+        this.getPrice();
+    }
+    getPrice(){
+        var newData = [];
+        console.log(newData);
+        // newData = data.slice(0);
+        newData = this.state.db.slice(0);
+        console.log(newData);
+            newData.map(function(stock){
+                var url = 'http://finance.yahoo.com/webservice/v1/symbols/'+ stock.symbol + '/quote?format=json&view=detail';
+                fetch(url)
+                .then((response) => response.json())
+                .then((jsonResponse) => {
+                    var price = jsonResponse.list.resources[0].resource.fields.price;
+                    stock.price = parseFloat(price).toFixed(2);
+                })
+                .catch((error) => {
+                    Alert.alert(
+                        'An error has occurred',
+                        'Please try again',
+                        [
+                            {text: 'Okay', onPress: () => console.log('OK Pressed')},
+                        ]
+                    );
+                });
+            });
+            this.setState({
+                dataSource: this.state.dataSource.cloneWithRows(newData),
+                db: newData,
+            });
+            //dataSource updates display after rendering and after I click on an element
+            //need the display to update before rendering and without clicking
     }
 
     _renderHeader(){
@@ -68,6 +106,12 @@ class MainWindow extends React.Component {
         });
     }
 
+    onRefresh(){
+        this.setState({refreshing: true});
+        this.getPrice();
+        this.setState({refreshing: false});
+    }
+
     render() {
         return (
             <View style={styles.container}>
@@ -75,10 +119,17 @@ class MainWindow extends React.Component {
 
                 </View>
                 <ListView
+                refreshControl={
+                    <RefreshControl
+                        refreshing={this.state.refreshing}
+                        onRefresh={this.onRefresh.bind(this)}
+                    />
+                }
                     style = {styles.listViewContainer}
                     dataSource = {this.state.dataSource}
                     renderRow={(rowData, sectionID, rowID) =>
-                        <StockItem stock={rowData}
+                        <StockItem
+                            stock={rowData}
                             onPress={() => this.props.onPress(rowData)}
                         />
                     }
