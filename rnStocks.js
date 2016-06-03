802acee64f92bcf387912b9cc01c096bb40d01ed
James <jbkuczma@buffalo.edu>
2016-06-02 20:11:46 -0400

    trying to use router-flux for navigation. seems to work well. will remove old, commented out code when I decide if this is the path i want to follow

--- rnStocks.js
+++ rnStocks.js
@@ -4,67 +4,82 @@ import React, {
 } from 'react';
 
 import {
-  Alert,
   AppRegistry,
-  Image,
   ListView,
   StyleSheet,
   Text,
   View,
   TouchableHighlight,
-  RefreshControl,
-  TouchableWithoutFeedback,
-  ScrollView,
   Navigator,
 } from 'react-native';
+
+import { Router, Scene } from 'react-native-router-flux';
 
 import MainWindow from './App/Views/Main';
 import EditStocks from './App/Views/EditStocks';
 import StockItemInfo from './App/Views/StockItemInfo';
 import styles from './App/Styles/styles';
 
-var NavigationBarRoute = {
-    LeftButton(route, navigator, index, navState){
-        if(index > 0){
-            return(
-                <TouchableHighlight
-                    underlayColor='transparent'
-                    onPress={() => {if(index > 0){navigator.pop() } }}
-                >
-                <Text style={styles.leftNavButton}> Back </Text>
-                </TouchableHighlight>
-            )
-        }else{
-            return null;
-        }
-    },
-    RightButton(route, navigator, index, navState){
-        return null;
-    },
-    Title(route, navigator, index, navState){
-        return null;
-    }
-};
+// var NavigationBarRoute = {
+//     LeftButton(route, navigator, index, navState){
+//         if(index > 0){
+//             return(
+//                 <TouchableHighlight
+//                     underlayColor='transparent'
+//                     onPress={() => {if(index > 0){navigator.pop() } }}
+//                 >
+//                 <Text style={styles.leftNavButton}> Back </Text>
+//                 </TouchableHighlight>
+//             )
+//         }else{
+//             return null;
+//         }
+//     },
+//     RightButton(route, navigator, index, navState){
+//         return null;
+//     },
+//     Title(route, navigator, index, navState){
+//         return null;
+//     }
+// };
+//
+// class rnStocks extends React.Component {
+//
+//     renderScene(route, navigator){
+//         return <route.component {...route.passProps} navigator = {navigator} />
+//     }
+//
+//     render(){
+//         return(
+//             <Navigator
+//                 initialRoute={{name: 'Main', component: MainWindow}}
+//                 renderScene = {this.renderScene}
+//                 navigationBar={
+//                     <Navigator.NavigationBar
+//                         routeMapper={NavigationBarRoute}
+//                     />
+//                 }
+//             />
+//         )
+//     }
+// }
 
 class rnStocks extends React.Component {
-
-    renderScene(route, navigator){
-        return <route.component {...route.passProps} navigator = {navigator} />
-    }
-
-    render(){
-        return(
-            <Navigator
-                initialRoute={{name: 'Main', component: MainWindow}}
-                renderScene = {this.renderScene}
-                navigationBar={
-                    <Navigator.NavigationBar
-                        routeMapper={NavigationBarRoute}
-                    />
-                }
-            />
-        )
-    }
+  render() {
+    return (
+      <Router navigationBarStyle={styles.navBar} titleStyle={styles.navTitle} sceneStyle={styles.routerScene}>
+        <Scene key="root">
+          <Scene key="Main" title="Your stocks" component={MainWindow} initial={true}/>
+          <Scene key="AddSearch" component={EditStocks} hideNavBar={false}/>
+          <Scene key="StockItemInfo" component={StockItemInfo} hideNavBar={false}/>
+        </Scene>
+      </Router>
+    )
+  }
 }
+
+var styles1 = StyleSheet.create({
+
+});
 
 module.exports = rnStocks;
