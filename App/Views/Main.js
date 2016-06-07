892b6cc3857c3cde2fb74e0074be5c69dd117750
James <jbkuczma@buffalo.edu>
2016-06-07 15:47:25 -0400

    committing this horrific code now and  will look over later

--- App/Views/Main.js
+++ App/Views/Main.js
@@ -47,15 +47,17 @@ class MainWindow extends React.Component {
             dataSource: ds.cloneWithRows(data),
             refreshing: false,
             db: data,
+            loaded: false,
         };
+        this.getPrice = this.getPrice.bind(this);
     }
 
+//FIX
     componentWillMount(){
-        this.getPrice();
-    }
-    getPrice(){
+        var newStock = [];
         let newData = this.state.db.slice(0);
-            newData.map(function(stock){
+            newData.map(function(stock, index){
+                newStock = stock;
                 var url = 'http://finance.yahoo.com/webservice/v1/symbols/'+ stock.symbol + '/quote?format=json&view=detail';
                 fetch(url)
                 .then((response) => response.json())
@@ -74,20 +76,158 @@ class MainWindow extends React.Component {
                     );
                 });
             });
-            this.setState({
-                dataSource: this.state.dataSource.cloneWithRows(newData),
-                db: newData,
+    }
+//FIX
+//PROBLEMS FROM HERE....
+    // componentDidMount(){
+    //     var promise = this.getPrice();
+    //     promise.then(function(val){
+    //         console.log(val);
+    //         //GET WARNING SAYING THAT VAL IS UNDEFINED AND CAN'T SET STATE OF UNDEFINED
+    //         this.setState({
+    //             db: val,
+    //             dataSource: this.state.dataSource.cloneWithRows(val),
+    //             loaded: true,
+    //         });
+    //     });
+    // }
+//FIX
+    getPrice() {
+        var newData = this.state.db.slice();
+        var newStock = [];
+        // newData.forEach(function(stock,index) {
+        //     var temp = JSON.parse(JSON.stringify(newData[index]));
+        //     var url = 'http://finance.yahoo.com/webservice/v1/symbols/'+ temp.symbol + '/quote?format=json&view=detail';
+        //         fetch(url)
+        //         .then((response) => response.json())
+        //         .then((jsonResponse) => {
+        //             var price = jsonResponse.list.resources[0].resource.fields.price;
+        //             var change = parseFloat(jsonResponse.list.resources[0].resource.fields.change).toFixed(2);
+        //             temp.price = price;
+        //             temp.change = change;
+        //             newStock.push(temp);
+        //             return newStock;
+        //         })
+        //         .then((stocks) => {
+        //             if(index === newData.length-1){
+        //                 // return stocks;
+        //                 if(stocks !== undefined){
+        //                     this.setState({
+        //                         db: stocks,
+        //                         dataSource: this.state.dataSource.cloneWithRows(stocks),
+        //                         loaded: true,
+        //                     });
+        //                 }
+        //             }
+        //         })
+        //         .catch((error) => {
+        //             Alert.alert(
+        //                 'An error has occurred',
+        //                 'Please try again',
+        //                 [
+        //                     {text: 'Okay', onPress: () => console.log('OK Pressed')},
+        //                 ]
+        //             );
+        //         });
+        // });
+
+        var promise = new Promise(
+            function(resolve,reject){
+                newData.forEach(function(stock,index) {
+                    var temp = JSON.parse(JSON.stringify(newData[index]));
+                    var url = 'http://finance.yahoo.com/webservice/v1/symbols/'+ temp.symbol + '/quote?format=json&view=detail';
+                    fetch(url)
+                    .then((response) => response.json())
+                    .then((jsonResponse) => {
+                        var price = parseFloat(jsonResponse.list.resources[0].resource.fields.price).toFixed(2);
+                        var change = parseFloat(jsonResponse.list.resources[0].resource.fields.change).toFixed(2);
+                        temp.price = price;
+                        temp.change = change;
+                        newStock.push(temp);
+                        return newStock;
+                    })
+                    .then((stocks) => {
+                        if(index === newData.length-1){
+                            resolve(stocks);
+                        }
+                    })
+                    .catch((error) => {
+                        Alert.alert(
+                            'An error has occurred',
+                            'Please try again',
+                            [
+                                {text: 'Okay', onPress: () => console.log('OK Pressed')},
+                            ]
+                        );
+                    });
+
+                });
             });
-            //dataSource updates display after rendering and after I click on an element
-            //need the display to update before rendering and without clicking
+        return promise;
+
+
+
+
+        // console.log("after");
+        // console.log(newStock);
+        // return newData;
+        // var newStock = [];
+        // let newData = this.state.db.slice(0);
+        // console.log(newData);
+        //     newData.map(function(stock, index){
+        //         newStock = stock;
+        //         console.log(newStock);
+        //         var url = 'http://finance.yahoo.com/webservice/v1/symbols/'+ stock.symbol + '/quote?format=json&view=detail';
+        //         fetch(url)
+        //         .then((response) => response.json())
+        //         .then((jsonResponse) => {
+        //             var price = jsonResponse.list.resources[0].resource.fields.price;
+        //             newStock.price = parseFloat(price).toFixed(2);
+        //             newStock.change = parseFloat(jsonResponse.list.resources[0].resource.fields.change).toFixed(2);
+        //             // newStock.push(stock);
+        //             console.log(newStock);
+        //             console.log('------');
+        //         })
+        //         .catch((error) => {
+        //             Alert.alert(
+        //                 'An error has occurred',
+        //                 'Please try again',
+        //                 [
+        //                     {text: 'Okay', onPress: () => console.log('OK Pressed')},
+        //                 ]
+        //             );
+        //         });
+        //     });
+        //     return newData;
     }
 
     onRefresh(){
+        let _this = this;
         this.setState({refreshing: true});
-        this.getPrice();
-        this.setState({refreshing: false});
+        // this.getPrice();
+        console.log(_this.state.db);
+        var promise = this.getPrice();
+        promise.then(function(val){
+            //GET WARNING SAYING THAT VAL IS UNDEFINED AND CAN'T SET STATE OF UNDEFINED
+            //YET LOOKING IN THE CONSOLE SHOWS A VALUE AND val === undefined returns false
+            if(val !== undefined){
+                console.log('in');
+                _this.setState({
+                    db: val,
+                    dataSource: _this.state.dataSource.cloneWithRows(val),
+                    loaded: true,
+                });
+                console.log(_this.state.db);
+            }
+        })
+        .catch(function(error){
+            console.log(error);
+        });
+        this.setState({
+            refreshing: false
+        });
     }
-
+//.... TO HERE
     render() {
         return (
             <View style={styles.container}>
@@ -110,6 +250,7 @@ class MainWindow extends React.Component {
                         />
                     }
                 />
+
                 <View>
                     <Text style={styles.marketHours}>
                         {(() => {
